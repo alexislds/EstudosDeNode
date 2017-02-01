673bd28d02aed3fecbb2f48373c0c9d138764c51
Alexis Santos <alexis.luiz.dias@gmail.com>
2017-02-01 21:35:18 -0200

    TERMINADO PARTE 2

---
+++ public/main.js
@@ -0,0 +1,41 @@
+var update = document.querySelector('#update');
+var del    = document.querySelector('#delete');
+
+update.addEventListener('click', function(){
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Darth Vader',
+      'quote': 'I find your lack of faith disturbing.'
+    })
+  })
+  .then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(data => {
+    console.log(data);
+    window.location.reload();
+  })
+});
+
+del.addEventListener('click', function(){
+  fetch('quotes', {
+    method: 'delete',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Darth Vader'
+    })
+  })
+  .then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(data => {
+    console.log(data);
+    window.location.reload();
+  })
+});
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // requires das coisas necessarias
-const app = require('express')();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
 // não entendi para o que serve mas facilita pegar um POST e transformar em json
 app.use(bodyParser.urlencoded({extended:true}));
+
+// faz nosso node entender json data
+app.use(bodyParser.json());
+
+// diz que a pasta public pode ser vista e usar pela pasta views
+app.use(express.static('public'));
 
 // diz para usar a view engine do ejs
 app.set('view engine', 'ejs');
@@ -43,4 +50,44 @@ app.post('/quotes', (req, res) => {
     //redireciona para url raiz
     res.redirect('/');
   });
+});
+
+// quando fizer um put (atraves de form ou js)
+app.put('/quotes', (req, res) => {
+  //trabalha com o put request
+  db.collection('quotes').findOneAndUpdate(
+    {
+      name: 'Alex'
+    },
+    {
+      $set: {
+      name: req.body.name,
+      quote: req.body.quote
+      }
+    },
+    {
+      sort: {_id:-1},
+      upsert: true
+    },
+    (err, results) => {
+      if (err) {
+        return res.send(err);
+      }
+      res.send(results);
+    }
+  );
+});
+
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndDelete(
+    {
+      name: req.body.name
+    },
+    (err, results) => {
+      if (err) {
+        return res.send(500, err);
+      }
+      res.send('A darth vader quote got deleted');
+    }
+  );
 });
