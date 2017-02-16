e12d25fb3049cbd2701aedc1ebaba12a313d486e
Alexis Santos <alexis.luiz.dias@gmail.com>
2017-02-15 23:20:27 -0200

    Cadastro agora mostra se o usuario ja existe no banco

--- Financias/app/routes/cadastro.js
+++ Financias/app/routes/cadastro.js
@@ -2,23 +2,36 @@ const MongoClient = require('mongodb').MongoClient;
 const ObjectId    = require('mongodb').ObjectId;
 
 module.exports = (app, db) => {
+  let errorUsuario;
+
+  app.post('/cadastro/cadastrar', (req, res) => {
+
+    db.collection('usuarios').find({usuario : {$exists : true, $in : [req.body.usuario]}}).toArray((err, results) => {
+      if (err) {
+        console.log(err);
+      }
+
+      if (results.length == 0) {
+        db.collection('usuarios').save(req.body, (err, results) => {
+          if (err){
+            console.log(err);
+          }
+        });
+      }
+
+      if (results.length > 0) {
+        errorUsuario = "Usuario já cadastrado";
+      }
+
+      res.redirect('/cadastro');
+    });
+  });
 
   app.get('/cadastro', (req, res) => {
     let sessionUserName = req.session.userName;
 
-    res.render('cadastro', {sessionName: sessionUserName});
-  });
+    res.render('cadastro', {sessionName : sessionUserName, error : errorUsuario});
 
-  app.post('/cadastro/cadastrar', (req, res) => {
-
-    db.collection('usuarios').save(req.body, (err, results) => {
-      if (err){
-        console.log(err);
-      }
-
-      res.redirect('/login');
-    })
-
-
+    errorUsuario = null;
   });
 }
